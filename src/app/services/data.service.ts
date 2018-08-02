c399035da833a3271518467e328be9aab39dd590
Emiton <emiton_alves@my.uri.edu>
2018-08-02 17:25:57 -0400

    Running through some basics

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(public http:Http) { 
+    console.log("Data Service Connected...");
+  }
+
+  getPosts(){
+    return this.http.get('http://jsonplaceholder.typicode.com/posts')
+      .map(res => res.json());
+  }
+
+
+}
